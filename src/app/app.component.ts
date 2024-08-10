40ff6bfff26f20b52817751fa4220509f84b699c
Mathieu Maes <mathieu@zimmo.be>
2024-08-10 09:28:58 +0200

    Add splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SplashComponent} from "../components/splash/splash.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/components/splash/splash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splash',
+  standalone: true,
+  imports: [],
+  templateUrl: './splash.component.html',
+  styleUrl: './splash.component.scss'
+})
+export class SplashComponent {
+
+}

66ac0f7e8b80ade28dab7462a4d8a3799ee9e6dc
Mathieu Maes <mathieu@zimmo.be>
2024-08-10 10:13:23 +0200

    Add about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SplashComponent} from "../components/splash/splash.component";
+import {AboutComponent} from "../components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SplashComponent],
+  imports: [RouterOutlet, SplashComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

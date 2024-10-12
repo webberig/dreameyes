06ff6bb1c42179d4388cb81bd3d3582aa39d3866
Mathieu Maes <mathieu@zimmo.be>
2024-10-12 15:12:50 +0200

    Add about background image with parallax

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -1,6 +1,8 @@
-import {Component, inject} from '@angular/core';
+import {Component, ElementRef, inject, PLATFORM_ID} from '@angular/core';
 import {Dialog} from "@angular/cdk/dialog";
 import {MemberDialogComponent} from "../member-dialog/member-dialog.component";
+import {isPlatformBrowser} from "@angular/common";
+import {animationFrames, map} from "rxjs";
 
 @Component({
   selector: 'app-about',
@@ -12,6 +14,27 @@ import {MemberDialogComponent} from "../member-dialog/member-dialog.component";
 export class AboutComponent {
 
   private dialog = inject(Dialog);
+
+  private element: HTMLElement = inject(ElementRef).nativeElement;
+
+  constructor() {
+    if (isPlatformBrowser(inject(PLATFORM_ID))) {
+      this.setupParallax();
+    }
+  }
+
+  private setupParallax () {
+    animationFrames().pipe(
+      map(() => {
+        const scrollY = this.element.getBoundingClientRect().top
+        const elementHeight = this.element.clientHeight;
+        const windowHeight = window.innerHeight;
+        return (scrollY * -1) + ((windowHeight - elementHeight) / 2);
+      })
+    ).subscribe(scrollOffset => {
+      this.element.style.setProperty('--scroll', scrollOffset + 'px');
+    });
+  }
 
   openMember(member: string) {
     this.dialog.open(MemberDialogComponent, {

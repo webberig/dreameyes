cbad4eef513f4d7c92ade0bf37cc05c31fb9200c
Mathieu Maes <mathieu@zimmo.be>
2024-10-12 11:16:52 +0200

    Add news component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {SplashComponent} from "../components/splash/splash.component";
 import {AboutComponent} from "../components/about/about.component";
 import {MusicComponent} from "../components/music/music.component";
 import {ContactComponent} from "../components/contact/contact.component";
+import {NewsComponent} from "../components/news/news.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SplashComponent, AboutComponent, MusicComponent, ContactComponent],
+  imports: [RouterOutlet, SplashComponent, AboutComponent, MusicComponent, ContactComponent, NewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
